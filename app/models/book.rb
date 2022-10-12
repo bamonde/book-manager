80b02ba7720c2b391a7c9ea9fec81a4e4849ec50
Herbertt Bamonde <hpbamonde@gmail.com>
2022-10-12 09:31:20 -0300

    Enabling ActiveStorage and adding cover for book.

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,8 @@ class Book < ApplicationRecord
   belongs_to :publisher
   has_and_belongs_to_many :authors
 
+  has_one_attached :cover
+
   validates :title, presence: true
   validates :genre, presence: true
   validates :release_date, presence: true
