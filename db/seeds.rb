6690bb23fbaaabb48e6418e27dd2a2a83499cd97
Herbertt Bamonde <hpbamonde@gmail.com>
2022-10-13 23:33:39 -0300

    Decreasing quantity of books created by seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+puts 'Running seeds'
+
 (1..5).each { Publisher.create(name: Faker::Book.publisher) }
 (1..10).each { Author.create(name: Faker::Book.author) }
 
 publishers = Publisher.all
 authors = Author.all
 
-(1..100).each do
+(1..30).each do
   book = Book.create(
     title: Faker::Book.title,
     genre: Faker::Book.genre,
